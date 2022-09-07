b07316b0bf1bc934a5c4314e71718cdc4af3f3af
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-09-07 10:10:37 +0200

    test: Add new testing functions.

---
+++ read_test.go
@@ -0,0 +1,55 @@
+package updenv_test
+
+import (
+	"github.com/gowizzard/updenv"
+	"reflect"
+	"testing"
+	"testing/fstest"
+)
+
+// TestRead is to test the read method with table driven tests
+func TestRead(t *testing.T) {
+
+	tests := []struct {
+		data     []byte
+		expected map[string][]byte
+	}{
+		{
+			data: []byte("# Dummy access data\nUSERNAME=admin\nPASSWORD=admin123"),
+			expected: map[string][]byte{
+				"USERNAME": []byte("USERNAME=admin"),
+				"PASSWORD": []byte("PASSWORD=admin123"),
+			},
+		},
+		{
+			data: []byte("URL=https://www.test.com/api/v1\nTOKEN=mySecret123456789"),
+			expected: map[string][]byte{
+				"URL":   []byte("URL=https://www.test.com/api/v1"),
+				"TOKEN": []byte("TOKEN=mySecret123456789"),
+			},
+		},
+	}
+
+	for _, value := range tests {
+
+		c := updenv.Config{
+			Filesystem: fstest.MapFS{
+				".env": {
+					Data: value.data,
+				},
+			},
+			Path: ".env",
+		}
+
+		err := c.Read()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !reflect.DeepEqual(value.expected, c.Entries) {
+			t.Fatalf("expected: \"%s\", got: \"%s\"", value.expected, c.Entries)
+		}
+
+	}
+
+}
---
+++ update_test.go
@@ -0,0 +1,59 @@
+package updenv_test
+
+import (
+	"github.com/gowizzard/updenv"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+// TestUpdate is to test the update method with table driven tests
+func TestUpdate(t *testing.T) {
+
+	tests := []struct {
+		path    string
+		bytes   []byte
+		entries map[string][]byte
+		updates map[string]string
+	}{
+		{
+			path:  filepath.Join(os.TempDir(), ".env"),
+			bytes: []byte("USERNAME=admin\nPASSWORD=admin123"),
+			entries: map[string][]byte{
+				"USERNAME": []byte("USERNAME=admin"),
+				"PASSWORD": []byte("PASSWORD=admin123"),
+			},
+			updates: map[string]string{
+				"USERNAME": "gowizzard",
+			},
+		},
+		{
+			path:  filepath.Join(os.TempDir(), ".env"),
+			bytes: []byte("URL=https://www.test.com/api/v1\nTOKEN=mySecret123456789"),
+			entries: map[string][]byte{
+				"URL":   []byte("URL=https://www.test.com/api/v1"),
+				"TOKEN": []byte("TOKEN=mySecret123456789"),
+			},
+			updates: map[string]string{
+				"TOKEN": "myNewSecret123456789",
+			},
+		},
+	}
+
+	for _, value := range tests {
+
+		c := updenv.Config{
+			Path:    value.path,
+			Bytes:   value.bytes,
+			Entries: value.entries,
+		}
+
+		c.Updates = value.updates
+		err := c.Update()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+	}
+
+}
